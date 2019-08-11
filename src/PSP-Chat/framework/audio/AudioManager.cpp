7d5ef226a7d90c16b4748dbc0bfb70518a2522d7
Nathan Bourgeois <NT-Bourgeois-Iridescence-Technologies@users.noreply.github.com>
2019-08-11 17:43:02 -0400

    Added audio (non-build!)

---
+++ src/PSP-Chat/framework/audio/AudioManager.cpp
@@ -0,0 +1,21 @@
+#include "AudioManager.hpp"
+
+namespace audio{
+    void AudioManager::Init()
+    {
+	    VirtualFileInit();
+	    oslInitAudio();
+    }
+
+    void AudioManager::Update()
+    {
+	    oslAudioVSync();
+    }
+
+    void AudioManager::PlaySound(OSL_SOUND * sound, SoundChannel channel)
+    {
+	    oslPlaySound(sound, channel);
+    }
+
+    AudioManager g_AudioManager = AudioManager();
+}
