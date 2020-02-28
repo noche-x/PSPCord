a46a784dd1ccee6b1b14e8e938477df635156483
noche <micheal4826@tuta.io>
2020-02-28 20:43:34 +0300

    Console Gui and Send Error?
    pspDebugScreenPrintf() and idk about the send error yet...

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,6 @@ PSP_HEAP_SIZE_MAX();
 // make it easier to type
 #define pspprint pspDebugScreenPrintf
 
-#include <iostream>
-#include <fstream>
-
-namespace Logger
-{
-    std::ofstream m_out;
-
-    void Init(const char* filename) {
-        if (std::ifstream(filename).good())
-            std::remove(filename);
-
-        m_out.open(filename, std::ofstream::out | std::ofstream::app );
-    }
-
-    void Log(const char* msg) {
-        m_out << msg << std::endl;
-        m_out.flush();
-    }
-
-    void Log(int msg) {
-        m_out << msg << std::endl;
-        m_out.flush();
-    }
-
-    void Close() {
-        m_out.close();
-    }
-} // namespace Logger
-
 extern "C"
 {
 	char buffer[1024] = "xdtbrh bxhdtrgbzrdsg";
@@ -62,47 +33,43 @@ extern "C"
 	}
 }
 
-const char *error_text = "";
-
-static __inline__ unsigned int sceAllegrexWsbw(unsigned int x)
+bool connect()
 {
-	return (((x & 0xFF) << 24) | ((x & 0xFF00) << 8) | ((x >> 8) & 0xFF00) | ((x >> 24) & 0xFF));
-}
-
-static __inline__ unsigned int sceAllegrexWsbh(unsigned int x)
-{
-	return (((x << 8) & 0xFF00FF00) | ((x >> 8) & 0x00FF00FF));
-}
-
-void connect()
-{
-	error_text = "";
+	pspprint("[*] connecting to LAN server...\n");
 	// create a socket to connect to the server
 	int sock = socket(PF_INET, SOCK_STREAM, 0);
-
+	
 	// initialize the ip and the port
 	struct sockaddr_in name;
 	name.sin_family = AF_INET;
-	name.sin_port = sceAllegrexWsbh(35700);
+	name.sin_port = __builtin_bswap16(35700);
 
 	inet_pton(AF_INET, "192.168.2.190", &name.sin_addr.s_addr); //Protect your IP... don't show it to the world... use the NAT for your advantage
 
 	// connect to the server via the socket and the address
-	if (connect(sock, (struct sockaddr *)&name, sizeof(name)) < 0)
-		error_text = "connection error";
+	if (connect(sock, (struct sockaddr *)&name, sizeof(name)) < 0) {
+		pspprint("[!] connection error\n");
+		return false;
+	}
 
 	// XD , this is a random xd left by the developers of this test project
 
 	// send it through to socket
 	if (send(sock, buffer, sizeof(buffer), 0)) //Server reads a buffer of 1024
-		error_text = "sending error";
+	{
+		pspprint("[!] sending error\n");
+		return false;
+	}
 
 	// server sends an message aswell
-	if (recv(sock, recvbuf, sizeof(recvbuf), 0))
-		error_text = "recv error";
+	if (recv(sock, recvbuf, sizeof(recvbuf), 0)) {
+		pspprint("[!] recv error\n");
+		return false;
+	}
 
 	// since we dont need it anymore we can close the socket
 	close(sock);
+	return true;
 }
 
 //MAIN
@@ -112,79 +79,99 @@ int main(void)
 	pspDebugScreenInit();
 	setupExitCallback();
 
-    // Logger::Init("comeon.log");
-	// Logger::Log("STARTED");
+	/*
+    ____  _____ ____        ________          __ 
+   / __ \/ ___// __ \      / ____/ /_  ____ _/ /_
+  / /_/ /\__ \/ /_/ /_____/ /   / __ \/ __ `/ __/
+ / ____/___/ / ____/_____/ /___/ / / / /_/ / /_  
+/_/    /____/_/          \____/_/ /_/\__,_/\__/  
+                                                 
+	*/
+	pspprint("    ____  _____ ____        ________          __ \n");
+	pspprint("   / __ \/ ___// __ \      / ____/ /_  ____ _/ /_\n");
+	pspprint("  / /_/ /\__ \/ /_/ /_____/ /   / __ \/ __ `/ __/\n");
+	pspprint(" / ____/___/ / ____/_____/ /___/ / / / /_/ / /_  \n");
+	pspprint("/_/    /____/_/          \____/_/ /_/\__,_/\__/  \n");
+	pspprint("\n");
+	pspprint("[+] temporary console gui :(\n");
+	pspprint("======================================\n");
+
+	pspprint("[*] loading kernel modules\n");
 
 	//We need to load some basic kernel modules in order to use networking features
-	// Logger::Log(sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON)); //All sceNetCommands
-	// Logger::Log(sceUtilityLoadNetModule(PSP_NET_MODULE_INET)); //All sceInetCommands (including the socket library)
-	// Logger::Log(sceUtilityLoadNetModule(PSP_NET_MODULE_SSL)); //Unused, but you can use SSL functions if you need.
-	sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON);
-	sceUtilityLoadNetModule(PSP_NET_MODULE_INET);
-	sceUtilityLoadNetModule(PSP_NET_MODULE_SSL);
+	sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON);	//All sceNetCommands
+	sceUtilityLoadNetModule(PSP_NET_MODULE_INET); 	//All sceInetCommands (including the socket library)
+	sceUtilityLoadNetModule(PSP_NET_MODULE_SSL); 	//Unused, but you can use SSL functions if you need.
 	//Result stores our codes from the initialization process
 	int result = 0;
+	pspprint("[+] done\n\n");
 
+	pspprint("[*] initalizing inet and apctl\n");
 	result = sceNetInit(128 * 1024, 42, 0, 42, 0); //Creates the network manager with a buffer
 	if (result < 0)
 	{ //These If Blocks close the game on an error
 		sceKernelExitGame();
 	}
-	// Logger::Log(result);
 
 	result = sceNetInetInit(); //Initializes Inet
 	if (result < 0)
 	{
 		sceKernelExitGame();
 	}
-	// Logger::Log(result);
 
 	result = sceNetApctlInit(0x10000, 48); //Initializes Access Point Control
 	if (result < 0)
 	{
 		sceKernelExitGame();
 	}
-	// Logger::Log(result);
 
 	result = sceNetResolverInit(); //Initializes DNS resolver (unused)
 	if (result < 0)
 	{
 		sceKernelExitGame();
 	}
-	// Logger::Log(result);
 
 	result = sceNetApctlConnect(1); //Connects to your first (primary) internet connection.
 
-	// Logger::Log(result);
+	pspprint("[+] done\n\n");
 
 	SceCtrlData buttonInput;
 
 	sceCtrlSetSamplingCycle(0);
 	sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
 
+	int x_pos = pspDebugScreenGetX();
+	int y_pos = pspDebugScreenGetY();
 	//Displays connection status
 	int err;
-	while (1)
+	while (isRunning())
 	{
 		int state;
 		err = sceNetApctlGetState(&state);
 		if (err != 0)
 		{
-			pspprint("ERROR: sceNetApctlGetState returns $%x\n", err);
+			pspDebugScreenSetXY(x_pos, y_pos + 1);
+			pspprint("[!] sceNetApctlGetState returns $%x\n", err);
 			break;
 		}
 		if (state == 4)
 			break; // connected!
 
-		pspDebugScreenSetXY(0, 0);
-		pspprint("Connecting...");
-		pspprint("  connection state %d of 4\n", state);
+		pspDebugScreenSetXY(x_pos, y_pos);
+		pspprint("[*] connecting to primary internet connection...\n");
+		pspprint("[*] connection state %d of 4\n\n", state);
 		sceDisplayWaitVblankStart();
 	}
 
 	SceNetApctlInfo info;
 	sceNetApctlGetInfo(PSP_NET_APCTL_INFO_IP, &info);
 
+	pspprint("[+] sceNetApctlInfo -> strength %c\n", info.strength);
+	pspprint("[+] sceNetApctlInfo -> ssid %s\n", info.ssid);
+	pspprint("[+] sceNetApctlInfo -> name %s\n\n", info.name);
+
+	x_pos = pspDebugScreenGetX();
+	y_pos = pspDebugScreenGetY();
 	/**
 	 * The code above works and connects you to the local network
 	 * The code below is WIP and connects you to the server and sends a message
@@ -192,18 +179,21 @@ int main(void)
 	int d = 0;
 	while (isRunning())
 	{
+		bool doublebreak = false;
 		d++;
-		connect();
+		bool ret = connect();
 
 		while (isRunning())
 		{
-			pspDebugScreenSetXY(0, 0);
-			if (error_text = "")
-				pspprint("Connected! MESSAGE RECIEVED: %s \n", recvbuf); //THERE
+			pspDebugScreenSetXY(x_pos, y_pos);
+			if (ret)
+				pspprint("[+] connected! recieved message: %s\n\n", recvbuf);
 			else
-				pspprint("%s : %i \n", error_text, errno);
+				pspprint("[!] failed to connect errno: %i\n\n", errno);
 
-			pspprint("\n %i \n", d); //THERE
+			pspprint("[+] amount of connects: %i\n\n", d);
+			pspprint("[i] press up arrow to reconnect\n");
+			pspprint("[i] press down arrow to close the program\n\n");
 
 			sceCtrlPeekBufferPositive(&buttonInput, 1);
 
@@ -214,11 +204,27 @@ int main(void)
 					sceKernelDelayThread(1000000 / 7);
 					break;
 				}
+				if (buttonInput.Buttons & PSP_CTRL_DOWN) {
+					sceKernelDelayThread(1000000 / 7);
+					doublebreak = true;
+					break;
+				}
 			}
-
 			sceDisplayWaitVblankStart();
 		}
+
+		if (doublebreak) {
+			pspprint("[+] see ya next time bye\n\n");
+			break;
+		}
+		else
+		{
+			pspprint("[*] reconnecting...\n\n");
+		}
+		
 	}
+	
+	pspprint("[*] exitting...\n");
 
 	sceKernelExitGame();
 
